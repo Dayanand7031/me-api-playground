f80bfbe7f726953d57bb21e75fb1f74084312a81
Dayanand <nandd7031@gmail.com>
2025-09-10 20:47:50 +0530

    build frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000", // FastAPI runs on port 8000
+  baseURL: "https://me-api-playground-85ed.onrender.com", // FastAPI runs on port 8000
 });
 
 // ---- Profile CRUD ----
